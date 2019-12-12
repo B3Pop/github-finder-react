abc33a1294171d490555f9ff27876986b4853996
Brian Gass <briangass3@gmail.com>
2019-12-11 19:46:39 -0500

    Refactor class based components to functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  render() {
-    const { avatar_url, login, html_url } = this.props.user;
+const UserItem = props => {
+  const { avatar_url, login, html_url } = props.user;
 
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a className='btn btn-dark btn-sm my-1' href={html_url}>
-            more
-          </a>
-        </div>
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a className='btn btn-dark btn-sm my-1' href={html_url}>
+          more
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
